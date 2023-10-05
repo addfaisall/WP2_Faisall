93531b969717240b41d75f8b2d3ad2d2374bb0fd
addfaisall <afaisal21x@gmail.com>
2023-10-06 00:13:50 +0700

    Menambahkan file pada tugas pertemuan 3

---
+++ application/controllers/Matakuliah.php
@@ -0,0 +1,25 @@
+<?php
+class matakuliah extends CI_Controller
+{
+    function __construct(){
+        parent::__construct();
+        $this->load->helper('url');
+
+    }
+
+    public function index()
+    {
+        $this->load->view('view-form-matakuliah');
+    }
+    public function cetak()
+    {
+        $data = [
+            'kode' => $this->input->post('kode'),
+            'nama' => $this->input->post('nama'),
+            'sks' => $this->input->post('sks'),
+        ];
+
+        $this->load->view('view-data-matakuliah', $data);
+
+    }
+}
