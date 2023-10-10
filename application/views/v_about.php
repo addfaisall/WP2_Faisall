42a453f9109046be711e65d06dc9420a0cb5f7d4
addfaisall <afaisal21x@gmail.com>
2023-10-10 17:21:06 +0700

    menambahkan file pertemuan 4

---
+++ application/views/v_about.php
@@ -0,0 +1,24 @@
+<section>
+    <h1><?php echo $judul2 ?></h1>
+    <h4>Nama</h4>
+    <ul type="disc">
+        <li>Nama Depan : Adila</li>
+        <li>Nama Belakang : Faisal</li>
+    </ul>
+    <br>
+    <h4>Alamat</h4>
+    <ul type="none">
+      <li> Jalan Raya Ceger</li>
+    </ul>
+
+    <h4>Tempat Lahir</h4>
+    <ul type="none">
+      <li>DKI JAKARTA</li>
+    </ul>
+
+    <h4>Olah Raga Favorit</h4>
+    <ul type="square">
+       <li>ngelamun</li>
+       <li>ngegame</li>
+    </ul>
+</section>
