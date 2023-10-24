c5b33b4926043262360e57d0b55c7ada5f8730c8
addfaisall <afaisal21x@gmail.com>
2023-10-24 16:38:49 +0700

    menambahkan file pertemuan 5

--- application/controllers/Matakuliah.php
+++ application/controllers/Matakuliah.php
@@ -1,25 +1,25 @@
 <?php
 class matakuliah extends CI_Controller
 {
-    function __construct(){
-        parent::__construct();
-        $this->load->helper('url');
-
-    }
-
+  
     public function index()
     {
         $this->load->view('view-form-matakuliah');
     }
     public function cetak()
     {
-        $data = [
-            'kode' => $this->input->post('kode'),
-            'nama' => $this->input->post('nama'),
-            'sks' => $this->input->post('sks'),
-        ];
-
-        $this->load->view('view-data-matakuliah', $data);
-
+        $this->form_validation->set_rules('kode', 'Kode Matakuliah','required|min_length[3]', ['required' => 'Kode Matakuliah Harus diisi','min_lenght' => 'Kode terlalu pendek']);
+        $this->form_validation->set_rules('nama', 'Nama Matakuliah','required|min_length[3]', ['required' => 'Nama Matakuliah Harus diisi','min_lenght' => 'Nama terlalu pendek']);
+        $this->form_validation->set_rules('sks', 'sks','required|min_length[1]', ['required' => 'SKS Harus diisi ya ganteng','min_lenght' => 'sks tidak ditemukan']);
+        if ($this->form_validation->run() != true) {
+            $this->load->view('view-form-matakuliah');
+        } else {
+            $data = [
+                'kode' => $this->input->post('kode'),
+                'nama' => $this->input->post('nama'),
+                 'sks' => $this->input->post('sks')
+            ];
+            $this->load->view('view-data-matakuliah', $data);
+        }
     }
 }
